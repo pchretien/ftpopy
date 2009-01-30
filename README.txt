53244b8c18320e5f7e364a395baeb0a0fb1abda7
Philippe Chretien <philippe.chretien@gmail.com>
2009-01-30 11:06:38 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+-------------------------------------------------------------------------------
+-- ftpoPy --
+-------------------------------------------------------------------------------
+
+Python files:
+	- python/ft_main.py
+	
+Other files:
+	- README.txt 			this file
+	- LICENSE.txt 			GPL license
+	- .project				Eclipse project file
+	- .pydevproject			Eclipse PyDev project file
+	  
+This program allows a user to take control of a remote computer using email.
+The following code is the server part of the application. The client part is
+not part of this project. Any email client can be used.
+
+You will find the latest version of this code at the following address:
+http://github.com/pchretien
+
+You can contact me at the following email address:
+philippe.chretien@gmail.com
+
+Thank you,
+
+Philippe Chrétien
---
+++ python/ft_main.py
@@ -0,0 +1,31 @@
+## ftpoPy ##
+#
+# This program allows a user to take control of a remote computer using email.
+# The following code is the server part of the application. The client part is
+# not part of this project. Any email client can be used.  
+# Copyright (C) 2008,2009  Philippe Chretien
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# You will find the latest version of this code at the following address:
+# http://github.com/pchretien
+#
+# You can contact me at the following email address:
+# philippe.chretien@gmail.com
+
+import poplib
+import smtplib
+import email
+
