8000cc389603d89f4c19b8bce37e01c2066ac245
Philippe Chretien <philippe.chretien@gmail.com>
2009-01-30 14:36:23 -0500

    Very first functionnal version of the application.

--- python/ft_main.py
+++ python/ft_main.py
@@ -25,7 +25,85 @@
 # You can contact me at the following email address:
 # philippe.chretien@gmail.com
 
+import sys
 import poplib
 import smtplib
 import email
+import subprocess
+import time
 
+from email.mime.text import MIMEText
+
+if len(sys.argv) < 5:
+    print "usage: ft_main.py pop_server pop_username pop password smtp_server [polling_period]"
+    
+# POP3 server to receive messages from ...
+popServer = sys.argv[1]
+popUsername = sys.argv[2]
+popPassword = sys.argv[3]
+
+# SMTP server to send emails
+smtpServer = sys.argv[4]
+
+period = 15 # seconds
+if len(sys.argv) == 6:
+    period = int(sys.argv[5])
+
+
+def returnResponse(msg, response):
+    global smtpServer
+    
+    # Replace with the SMTP call or other ways of returning the response
+    reply = MIMEText(response)
+    reply['Subject'] = "RE:" + msg["Subject"]
+    reply['From'] = msg["To"]
+    reply['To'] = msg["From"]
+        
+    server = smtplib.SMTP(smtpServer)
+    server.set_debuglevel(1)
+    
+    # To and From reversed for the reply
+    server.sendmail(msg["To"], msg["From"], reply.as_string())
+    server.quit()
+
+def processMsg(msg):
+    try:
+        # msg._payload[0] is the text/plain content
+        for line in msg._payload[0]._payload.split('\n'):
+            if len(line) > 0:
+                # Execute the command ...
+                print "->", line
+                command = "cmd.exe /c " + line
+                 
+                p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) 
+                text = p.stdout.read()
+                p.wait()  
+                
+                returnResponse(msg, text)
+    except:
+        return False
+    
+    return True
+                   
+while True:
+    print "reading e-mails ..."
+    
+    M = poplib.POP3(popServer)
+    M.user(popUsername)
+    M.pass_(popPassword)
+    numMessages = len(M.list()[1])
+    for i in range(numMessages):
+        emailContent = ""
+        for s in M.retr(i+1)[1]:
+            emailContent += s
+            emailContent += "\n"
+        
+        msg = email.message_from_string(emailContent)
+        if processMsg(msg):
+            M.dele(i+1)
+            
+    M.quit()    
+    print "Done."
+    
+    time.sleep(period)
+    
